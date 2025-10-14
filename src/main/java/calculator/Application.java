04d6e85ff086bbc96170d258e7ae82e813b19a15
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-14 20:39:23 +0900

    feat(Application): 입력을 받고, DelimiterParser로 숫자배열을 생성하여 Calculator에 전달하는 main 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterParser;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        DelimiterParser delimiterParser = new DelimiterParser();
+
+        List<Integer> numbers = delimiterParser.parse(input);
+        Calculator calculator = new Calculator();
+        calculator.sum(numbers);
+        System.out.println("결과 : " + calculator.getSum());
     }
 }
